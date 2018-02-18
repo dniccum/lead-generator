033b2f25ebb373948b3dafdd5988e905d569463f
Doug Niccum <dniccumdesign@gmail.com>
2018-02-17 20:16:57 -0600

    Creates the lead model.

---
+++ api/models/Lead.js
@@ -0,0 +1,53 @@
+/**
+* Quiz.js
+*
+* @description :: This is the Lead model.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    // model definitions
+    type: {
+      type: 'string',
+      required: true,
+      enum: ['home', 'health', 'auto', 'life']
+    },
+    zipCode: {
+      type: 'string',
+      required: true,
+      numeric: true,
+      minLength: 5,
+      maxLength: 5
+    },
+    firstName: {
+      type: 'string',
+      required: true,
+      alpha: true
+    },
+    lastName: {
+      type: 'string',
+      required: true,
+      alpha: true
+    }
+  },
+  validationMessages: {
+    type: {
+      required: "Please indicate the type of lead this is.",
+      enum: 'Please select "home, health, auto," or "life."'
+    },
+    zipCode: {
+      required: "Please include a zip code for this lead.",
+      numeric: 'Please enter a zip postal code.',
+      minLength: 'The zip code that you have entered is not long enough.',
+      maxLength: 'The zip code that you have entered is too long enough.'
+    },
+    firstName: {
+      required: "Please include a first name for this lead.",
+    },
+    lastName: {
+      required: "Please include a last name for this lead.",
+    },
+  }
+};
