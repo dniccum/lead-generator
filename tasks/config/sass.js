e8354092fd83f1529b83146dfc0d997f48ee2bf5
Doug Niccum <dniccumdesign@gmail.com>
2018-02-17 20:13:22 -0600

    Adds the sass compilation step.

---
+++ tasks/config/sass.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+    grunt.config.set('sass', {
+        dev: {
+            files: [{
+                expand: true,
+                cwd: 'assets/styles/',
+                src: [
+                    'importer.scss'
+                ],
+                dest: '.tmp/public/styles/',
+                ext: '.css'
+            }]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+};
